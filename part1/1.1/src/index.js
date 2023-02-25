97055e5140b8ba1112f516b7fb63705b7a016dc2
Diana Pineda <dianipineda55@gmail.com>
2023-02-25 15:17:10 -0500

    part1, exercise1.5

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -1,26 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
@@ -28,7 +30,7 @@ const Header = (props) => {
   console.log("los props de Header son:", props);
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -37,9 +39,18 @@ const Content = (props) => {
   console.log("los props de Content son:", props);
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part
+        part={props.course.parts[0].name}
+        exercises={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercises={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
     </>
   );
 };
@@ -61,7 +72,7 @@ const Total = (props) => {
     <>
       <p>
         Number of exercises{" "}
-        {props.parts.forEach((element) => {
+        {props.course.parts.forEach((element) => {
           arrayExercises.push(element.exercises);
         })}
         {arrayExercises.reduce((acum, current) => acum + current)}
