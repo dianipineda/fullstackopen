1a08756d89c1df522b7d27a75c75e62c2ee51da5
Diana Pineda <dianipineda55@gmail.com>
2023-03-03 17:03:05 -0500

    part1, exercise1.12

--- part1/exercises/src/index.js
+++ part1/exercises/src/index.js
@@ -81,7 +81,7 @@ const Total = (props) => {
 };
 //ReactDOM.render(<App />, document.getElementById("root"));
 
-//Exercises part1 d
+//Exercises part1 d 1.6 - 1.11
 const Title = ({ text }) => <h1>{text}</h1>;
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
@@ -144,4 +144,27 @@ const App2 = () => {
   );
 };
 
-ReactDOM.render(<App2 />, document.getElementById("root"));
+//Exercises part1 d 1.12 - 1.
+const App3 = () => {
+  const [selected, setSelected] = useState(0);
+  const random = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+
+  return (
+    <>
+      <div>{anecdotes[selected]}</div>
+      <button onClick={random}>next anecdote</button>
+    </>
+  );
+};
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+ReactDOM.render(<App3 />, document.getElementById("root"));
